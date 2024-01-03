d0201b975adf88eaea7d974716e194eb88792c67
VL.dilankarw <VL.dilankarashmika10@gmail.com>
2024-01-03 10:49:03 +0530

    new update

--- homeshop.php
+++ homeshop.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -48,7 +48,38 @@
     </section>
     <script src="jquary.js"></script>
     <script src="slick.js"></script>
-
     <script type="text/javascript"></script>
+
+    $('.popular-brands-content').slick({
+  lozyLoad:'ondemand',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
 </body>
-</html>
+</html> -->
